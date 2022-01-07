5b35738a00c7042bb7e56e7e958d7e8797214cd2
Wilangel <wilangeldj@gmail.com>
2022-01-07 11:53:33 -0500

    video 7 debug

--- app/src/main/java/com/udc/wilangel/MainActivity.java
+++ app/src/main/java/com/udc/wilangel/MainActivity.java
@@ -3,6 +3,7 @@ package com.udc.wilangel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int matematicas = 5;
+        int quimica = 5;
+        int fisica = 5;
+        int promedio = 0;
+
+
+
+        promedio = (matematicas + quimica + fisica)/3;
+
+        if(promedio >= 6){
+            Toast.makeText(this, "aprobado", Toast.LENGTH_SHORT).show();
+        }else if(promedio<=5){
+            Toast.makeText(this, "reprobado", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
