adde7917bf808ba696ccb5ccd11c7c6818e056c8
Wilangel <wilangeldj@gmail.com>
2022-01-07 12:47:56 -0500

    video 10 APP SUMA

--- app/src/main/java/com/udc/wilangel/MainActivity.java
+++ app/src/main/java/com/udc/wilangel/MainActivity.java
@@ -3,29 +3,42 @@ package com.udc.wilangel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1 ;
+    private EditText et2;
+    private TextView tv1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int matematicas = 5;
-        int quimica = 5;
-        int fisica = 5;
-        int promedio = 0;
+        et1 = (EditText) findViewById(R.id.txtNum1);
+        et2 = (EditText) findViewById(R.id.txtNum2);
+        tv1 = (TextView) findViewById(R.id.tvResultado);
 
-
-
-        promedio = (matematicas + quimica + fisica)/3;
-
-        if(promedio >= 6){
-            Toast.makeText(this, "aprobado", Toast.LENGTH_LONG).show();
-        }else if(promedio<=5){
-            Toast.makeText(this, "reprobado", Toast.LENGTH_LONG).show();
         }
+    // este metodo realiza la suma
+
+    public void sumar(View view){
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int suma = num1 + num2;
+
+        String Resultado = String.valueOf(suma);
+
+        tv1.setText(Resultado);
     }
 
 }
